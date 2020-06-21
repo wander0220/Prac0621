d670e00b17333a71c0207daeae238b34fed94e0f
Kim Suhyeon <s2018s26@e-mirim.hs.kr>
2020-06-21 13:31:28 +0900

    youngOneHee Background print ...★

--- Prac0621/game_background.cpp
+++ Prac0621/game_background.cpp
@@ -8,7 +8,8 @@ Background::Background()
 }
 void Background::Update() 
 {
-
+    backgroundY += 10;
+    backgroundY = (int)backgroundY % 680;
 }
 void Background::Render()
 {
@@ -23,6 +24,23 @@ void Background::Render()
 
     D3DXVECTOR3 pos(backgroundX, backgroundY, 0);
 
-    newElement->g_pTextSprite->Draw(newElement->g_pTexture, &rect, nullptr, &pos, D3DCOLOR_XRGB(255, 255, 255));
+    newElement->g_pTextSprite->Draw(newElement->g_pTexture, &rect, nullptr
+        , &pos, D3DCOLOR_XRGB(255, 255, 255));
     newElement->g_pTextSprite->End();
+
+
+    TextureElements* newElement2 = texturemanager.GetTexture(GAME_BACKGROUND);
+    newElement2->g_pTextSprite->Begin(D3DXSPRITE_ALPHABLEND);
+
+    RECT rect2;
+    rect2.left = 0;
+    rect2.top = 0;
+    rect2.right = 635;
+    rect2.bottom = 680;
+
+    D3DXVECTOR3 pos2(backgroundX, backgroundY-680, 0);
+
+    newElement2->g_pTextSprite->Draw(newElement2->g_pTexture, &rect, nullptr
+        , &pos2, D3DCOLOR_XRGB(255, 255, 255));
+    newElement2->g_pTextSprite->End();
 }
